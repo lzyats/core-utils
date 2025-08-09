95a550612f217fa675123fa3916f4960be8284f6
lzyats <lzyats@qq.com>
2025-08-09 08:40:32 +0800

    2.0.0

--- src/main/java/com/platform/common/web/exception/ErrorAttributesCustom.java
+++ src/main/java/com/platform/common/web/exception/ErrorAttributesCustom.java
@@ -1,5 +1,6 @@
+package com.platform.common.web.exception;
+
 import com.platform.common.web.domain.AjaxResult;
-import org.springframework.boot.web.error.ErrorAttributeOptions; // 注意导入正确的包
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.WebRequest;
@@ -10,9 +11,8 @@ import java.util.Map;
 public class ErrorAttributesCustom extends DefaultErrorAttributes {
 
     @Override
-    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
-        // 这里可以根据需要处理 options（例如是否包含堆栈信息）
-        return AjaxResult.fail(); // 你的自定义返回
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+        return AjaxResult.fail();
     }
 
 }
